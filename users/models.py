447124c515e14b8523b00610be74fe961bf0cebc
Manuel-bit <emmanuel_njuguna@outlook.com>
2020-09-15 10:25:34 +0300

    add profile pic path

--- users/models.py
+++ users/models.py
@@ -17,7 +17,7 @@ class Tutor(models.Model):
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     full_name = models.CharField(max_length=200, null=False)
-    image = models.ImageField( upload_to='profile_pics', null=True)
+    image = models.ImageField( default="profile_pics/default.jpg" ,upload_to='profile_pics')
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     
 
